e09aee86a60126b83f57dbf29c90c2722bd2fa4d
Naylane Ribeiro <naylaneribeiro4@gmail.com>
2024-11-26 10:57:58 -0300

    🚚 chore: renomeando arquivo do mouse

--- src/mouse.c
+++ src/mouse.c
@@ -3,21 +3,12 @@
 #include <linux/input.h>
 #include <unistd.h>
 
+
 int abre_mouse();
 void le_mouse(int fd);
-int teste1();
+int teste();
 
-int main() {
-    int fd = abre_mouse();
-    if (fd != -1) (
-        le_mouse(fd);
-        close(fd);
-    )
-    
-    return 0;
-}
 
-// abre o driver em modo leitura
 int abre_mouse() {
     int fd = open("/dev/input/event0", O_RDONLY);
     if (fd == -1) {
@@ -49,15 +40,29 @@ void le_mouse(int fd) {
     }
 }
 
-/*
-struct input_event {
-struct timeval time;
-__u16 type;
-__u16 code;
-__s32 value;
-};
-*/
-int teste1() {
+void le_mouse1(int fd) {
+    struct input_event ie;
+    int x = 0, y = 0;
+    
+    while (1) {
+        if (read(fd, &ie, sizeof(ie)) != sizeof(ie)) {
+            perror("Erro ao ler o evento");
+            break;
+        }
+
+        if (ie.type == EV_REL) {
+            if (ie.code == REL_X) {
+                x = ie.value;
+                //printf("Movimento horizontal: %d\n", x);
+            } else if (ie.code == REL_Y) {
+                y = ie.value;
+                //printf("Movimento vertical: %d\n", y);
+            }
+        }
+    }
+}
+
+int teste() {
     int fd = open("/dev/input/event0", O_RDONLY);
     if (fd == -1) {
         perror("Erro");
@@ -72,3 +77,22 @@ int teste1() {
     close(fd);
     return 0;
 }
+
+int main() {
+    int fd = abre_mouse();
+    if (fd != -1) {
+        le_mouse(fd);
+        close(fd);
+    }
+    
+    return 0;
+}
+
+/*
+struct input_event {
+struct timeval time;
+__u16 type;
+__u16 code;
+__s32 value;
+};
+*/
