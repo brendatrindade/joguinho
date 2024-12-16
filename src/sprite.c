bcc30b1859718d9e98a09d18e6354d8b144ad908
brendatrindade <brendatrindde@gmail.com>
2024-12-16 01:50:01 -0300

    ✨ feat: add sprite.c

---
+++ src/sprite.c
@@ -0,0 +1,53 @@
+#include "proc_grafico.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+
+extern void exibe_sprite(uint8_t sp, uint32_t xy, uint16_t offset, uint8_t registrador);
+extern void altera_pixel_sprite(uint16_t cor, uint16_t endereco);
+
+#define largura_sprite 20
+#define altura_sprite 20
+
+uint16_t converte_em_bgr(uint8_t rgb);
+void cria_sprite(uint16_t end_base, uint16_t dados_do_sprite[altura_sprite][largura_sprite]);
+
+
+//Converte os dados da imagem de RGB para BGR 9 bits - formato da instrucao wsm
+uint16_t converte_em_bgr(uint8_t rgb) {
+    uint8_t r, g, b;
+    uint16_t bgr;
+
+    //Extrai cada tom (R, G e B) do formato RGB
+    r = (rgb >> 5) & 0b111; // 3 bits msb do vermelho
+    g = (rgb >> 2) & 0b111; // 3 bits do meio do verde
+    b = rgb & 0b11; // 2 bits lsb do azul
+    //ajusta o azul de 2 para 3 bits
+    b = b << 1;
+
+    //Coloca no formato BBB GGG RRR de 9 bits
+    bgr = ( (b << 6) | (g << 3) | r );
+
+    return bgr;
+}
+
+//Cria e armazena um sprite na memoria de sprites
+void cria_sprite(uint16_t end_base, uint16_t dados_do_sprite[altura_sprite][largura_sprite]) {
+    uint16_t cor[400]; //20x20 -> 400 pixels por sprite
+    int y, x;
+    int z = 0;
+    for ( y = 0; y < altura_sprite; y++) {
+        for ( x = 0; x < largura_sprite; x++) {
+            cor[z] = dados_do_sprite[y][x]; //Extrai a cor de cada pixel em 9 bits BGR
+            z++;
+        }
+    }
+    //Escreve cada pixel da matriz 20x20 na memoria de sprites
+    int i = 0;
+    while (i < 400){
+        altera_pixel_sprite(cor[i], end_base + i);
+        i++;
+        usleep(10000);
+    }
+}
